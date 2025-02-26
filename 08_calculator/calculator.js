c1a509280a9b2071f203a83846382f57258d2025
heavy-goose <133355952+heavy-goose@users.noreply.github.com>
2025-02-26 10:51:39 +0100

    Complete javascript-exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(((total, item) => total + item), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(((product, item) => product * item), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for (let i = 1; i <= a; i++) factorial *= i;
+  return factorial;
 };
 
 // Do not edit below this line
