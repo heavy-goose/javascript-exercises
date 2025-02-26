bb35b999c6c4460a8f9ef023aa70160cd7f6d6b4
heavy-goose <133355952+heavy-goose@users.noreply.github.com>
2025-02-26 11:49:22 +0100

    Complete javascript-exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = (new Date).getFullYear();
+    const sortedByAge = arr.sort((personA, personB) => {
+        return ((personB.yearOfDeath ? personB.yearOfDeath : currentYear) - personB.yearOfBirth) -
+        ((personA.yearOfDeath ? personA.yearOfDeath : currentYear) - personA.yearOfBirth)
+    });
+    return sortedByAge[0];
 };
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
