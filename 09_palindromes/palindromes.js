1a80bb82a6bf92d4fdb3d62f8fa35bc550ae897c
heavy-goose <133355952+heavy-goose@users.noreply.github.com>
2025-02-26 11:11:27 +0100

    Complete javascript-exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let processed = str
+    .toLowerCase()
+    .split("")
+    .filter(char => char.match(/[a-z0-9]/));
+    return processed.join("") === processed.reverse().join("");
 };
 
 // Do not edit below this line
